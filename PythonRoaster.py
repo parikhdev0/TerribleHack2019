6bf8ac38d96d8bd9ada31af278c778d383c8d274
Mrugank Upadhyay <32528837+DeltaEagle11022@users.noreply.github.com>
2019-11-09 14:04:54 -0500

    Update PythonRoaster.py

--- PythonRoaster.py
+++ PythonRoaster.py
@@ -1,11 +1,28 @@
 # Let the Roasting Begin
+# Later on, this will be integrated into Dyme where if someone you know is near you, it will automatically send them a roast
+
 import pull
+import pyperclip
+
+def Roast(person, age):
+    if age < 20:
+
+    elif age < 30:
+
+    elif age < 40:
+
+    elif age < 50:
+
+    else:
 
 n = 10
-roastees = pull.users(10)
+roastees = pull.users(n)
 
 for person in roastees:
-    pass
+    pyperclip.copy(Roast(person, person['age']))
+    #pull.send()
+    #time.sleep as worst case
+
 
 roasts = {
         'photography' : [],
@@ -21,4 +38,17 @@ roasts = {
         'fitness' : [],
         'camping' : [],
         'Netflix' : [],
-        'beach' : []}
+        'beach' : [],
+        'dog': [],
+        'adventures': [],
+        'snapchat': [],
+        'coffee': [],
+        'instagram': [],
+        'IG': [],
+        'boys': [],
+        'jokes': [],
+        'tattoos': [],
+        'drinking': [],
+        'party': []}
+
+
